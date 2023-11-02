5178b91a298d67225f77883851aefe3eb81dded9
Mason Wooley <mason.wooley@gmail.com>
2023-11-02 01:54:21 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Lodestone {
+    public class Game1 : Game {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private RenderTarget2D gameWindow;
+
+        private Texture2D bush;
+        private Player player;
+
+        private KeyboardState currentKeyboardState;
+        private KeyboardState previousKeyboardState;
+
+        public Game1() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        // Called before any game frames to initialize subsystems
+        protected override void Initialize() {
+            // Set the size of the screen
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 720;
+            graphics.ApplyChanges();
+
+            // Content is drawn to the RenderTarget and then scaled up to the size of the window (x4)
+            gameWindow = new RenderTarget2D(GraphicsDevice, 320, 180);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            bush = Content.Load<Texture2D>("Graphics/World/Bush");
+            player = new Player(new Vector2(100, 50), Content.Load<Texture2D>("Graphics/Player/Player"), new Rectangle(0, 0, 64, 64), 60, 0.15f);
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime) {
+            currentKeyboardState = Keyboard.GetState();
+
+            player.Update(gameTime, currentKeyboardState, previousKeyboardState);
+
+            previousKeyboardState = currentKeyboardState;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            // First, draw to the RenderTarget
+            GraphicsDevice.SetRenderTarget(gameWindow);
+            GraphicsDevice.Clear(new Color(80, 80, 80));
+            
+            spriteBatch.Begin();
+            spriteBatch.Draw(bush, new Vector2(20, 20), Color.White);
+            player.Draw(gameTime, spriteBatch);
+            spriteBatch.End();
+
+            GraphicsDevice.SetRenderTarget(null);
+
+            // Then draw the RenderTarget to the screen scaled up
+            GraphicsDevice.Clear(Color.Black);
+            
+            // Setting PointClamp here keeps the pixel art from scaling blurry
+            spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            spriteBatch.Draw(gameWindow, Vector2.Zero, null, Color.White, 0f, Vector2.Zero, 4, SpriteEffects.None, 0f);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace Lodestone {
+    class Player {
+        public Vector2 Position;
+        public Texture2D Texture;
+        public Rectangle FramePosition;
+        public int FrameCount;
+        public float FrameTime;
+
+        // These can also be instantiated in the constructor
+        private int activeFrame = 0;
+        private float timer = 0.0f;
+        private float speed = 2.0f;
+        private Vector2 velocity = Vector2.Zero;
+
+        public Player(Vector2 position, Texture2D texture, Rectangle frame, int frameCount, float frameTime) {
+            Position = position;
+            Texture = texture;
+            FramePosition = frame;
+            FrameCount = frameCount;
+            FrameTime = frameTime;
+        }
+
+        public void Update(GameTime gameTime, KeyboardState currKeys, KeyboardState prevKeys) {
+            #region Animation
+            // Count the number of elapsed seconds between each call
+            timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Loop the animation from start
+            if (activeFrame >= FrameCount) {
+                activeFrame = 0;
+            }
+
+            // Update the activeFrame after the elapsed time has passed the frame animation time
+            if (timer >= FrameTime) {
+                activeFrame++;
+
+                // Then reset the timer
+                timer = 0.0f;
+            }
+
+            FramePosition.X = FramePosition.Width * activeFrame;
+            #endregion
+
+            #region Input
+            if (currKeys.IsKeyDown(Keys.Right)) {
+                velocity.X = 1;
+            }
+            else if (currKeys.IsKeyDown(Keys.Left)) {
+                velocity.X = -1;
+            }
+            else {
+                velocity.X = 0;
+            }
+
+            if (currKeys.IsKeyDown(Keys.Down)) {
+                velocity.Y = 1;
+            } else if (currKeys.IsKeyDown(Keys.Up)) {
+                velocity.Y = -1;
+            } else {
+                velocity.Y = 0;
+            }
+
+            // Only normalize the vector if it has magnitude, otherwise it divides by zero
+            if (velocity.Length() > 0) {
+                velocity.Normalize();
+            }
+
+            Position += velocity * speed;
+            #endregion
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
+            spriteBatch.Draw(Texture, Position, FramePosition, Color.White);
+        }
+    }
+}
